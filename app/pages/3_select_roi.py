50cd3116b54d567852c843558c4bc3dd27f670c9
Alex Liberzon <alexlib@users.noreply.github.com>
2023-03-04 17:45:45 +0200

    close to the solution using streamlit_image_coordinates Image.ImageDraw

--- app/pages/3_select_roi.py
+++ app/pages/3_select_roi.py
@@ -1,62 +1,60 @@
-from pathlib import Path
-
-import numpy as np
-import pandas as pd
 import streamlit as st
-from PIL import Image
-from streamlit_drawable_canvas import st_canvas
-from svgpathtools import parse_path
+from streamlit_image_coordinates import streamlit_image_coordinates
+from PIL import Image, ImageDraw
 
-# def full_app(frame_a):
 frame_a = st.session_state.frame_a
 
-st.sidebar.header("Configuration")
-st.markdown(
-    """
-Draw on the canvas, get the drawings back to Streamlit!
-* Configure canvas in the sidebar
-* In transform mode, double-click an object to remove it
-* In polygon mode, left-click to add a point, right-click to close the polygon, double-click to remove the latest point
-"""
+st.set_page_config(
+    page_title="Streamlit Image Coordinates: Image Update",
+    page_icon="🎯",
+    layout="wide",
 )
 
-with st.echo("below"):
-    # Specify canvas parameters in application
-    # drawing_mode = st.sidebar.selectbox(
-    #     "Drawing tool:",
-    #     ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
-    # )
-    drawing_mode = "rect"
-    stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-    if drawing_mode == 'point':
-        point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-    stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-    bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-    # bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-    realtime_update = st.sidebar.checkbox("Update in realtime", True)
+"# :dart: Streamlit Image Coordinates: Image Update"
+
+if "points" not in st.session_state:
+    st.session_state["points"] = []
 
 
-    
-    # Create a canvas component
-    canvas_result = st_canvas(
-        fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-        stroke_width=stroke_width,
-        stroke_color=stroke_color,
-        background_color=bg_color,
-        background_image=Image.fromarray(frame_a), #Image.open(bg_image) if bg_image else None,
-        update_streamlit=realtime_update,
-        height=150,
-        drawing_mode=drawing_mode,
-        point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
-        display_toolbar=st.sidebar.checkbox("Display toolbar", True),
-        key="full_app",
+"## Click on image"
+
+
+def get_ellipse_coords(point: tuple[int, int]) -> tuple[int, int, int, int]:
+    center = point
+    radius = 5
+    return (
+        center[0] - radius,
+        center[1] - radius,
+        center[0] + radius,
+        center[1] + radius,
     )
 
-    # Do something interesting with the image data and paths
-    if canvas_result.image_data is not None:
-        st.image(canvas_result.image_data)
-    if canvas_result.json_data is not None:
-        objects = pd.json_normalize(canvas_result.json_data["objects"])
-        for col in objects.select_dtypes(include=["object"]).columns:
-            objects[col] = objects[col].astype("str")
-        st.dataframe(objects)
+
+with st.echo("below"):
+    # with Image.open("kitty.jpeg") as img:
+    with Image.fromarray(frame_a) as img:
+        draw = ImageDraw.Draw(img)
+        # coords = get_ellipse_coords([])
+        # draw.rectangle(coords, fill=None, outline='yellow', width=1)
+
+        # # Draw an ellipse at each coordinate in points
+        for point in st.session_state["points"]:
+            coords = get_ellipse_coords(point)
+            draw.ellipse(coords, fill="white")
+            
+            value = streamlit_image_coordinates(img, key="pil")
+
+            if value is not None:
+                point = value["x"], value["y"]
+
+                if point not in st.session_state["points"]:
+                    st.session_state["points"].append(point)
+                    if len(st.session_state["points"]) == 2:
+                        draw.rectangle(st.session_state["points"][:2], fill=None, outline='yellow', width=1)
+                    
+                    st.experimental_rerun()
+                    
+        
+            
+                
+    # st.write(st.session_state["points"])
